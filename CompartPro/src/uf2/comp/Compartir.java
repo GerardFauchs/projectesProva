5ed725e38d994c7735c0cc28785cb4a264f394af
ecopika <ecopika@ecopika-Aspire-5732Z>
2014-02-07 19:31:28 +0100

    xcv

--- CompartPro/src/uf2/comp/Compartir.java
+++ CompartPro/src/uf2/comp/Compartir.java
@@ -3,7 +3,7 @@ package uf2.comp;
 public class Compartir {
 	
 	public static void main(String[] args){
-		System.out.println("Hola cotm vaf?");
+		System.out.println("Hola cotm vafxfasdf?");
 		System.out.println("Nuuss  ");
 
 	}
